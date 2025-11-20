0049888e813c0cde10c54b240009a7d10c19673b
BHAVESH SANJAY PATIL <154696194+bhavesh-patil45@users.noreply.github.com>
2025-11-20 16:22:14 +0530

    Objects and state management

---
+++ Lecture 40 Objects and State Management/script.js
@@ -0,0 +1,47 @@
+// const products = [
+//     {
+//         title: "product 1",
+//         price: 30,
+//     },
+//     {
+//         title: "product 2",
+//         price: 20,
+//     }, {
+//         title: "product 3",
+//         price: 30,
+//     }, {
+//         title: "product 4",
+//         price: 40,
+//     },
+//     {
+//         title: "product 5",
+//         price: 50,
+//     },
+// ];
+
+// const galleryElement = document.querySelector(".gallery")
+// const showBtn = galleryElement.firstElementChild;
+// showBtn.addEventListener("click", (e) => {
+// showBtn.computedStyleMap.display = "none"
+//     e.preventDefault();
+//     products.forEach((product) => {
+//         const newProductElement = document.createElement("div");
+//         newProductElement.classList.add("card")
+//         newProductElement.innerHTML =  
+//         div.classList.add("card");
+
+
+
+//         div.innerHTML = `<div class="title">${product.title} </div>
+
+//                         <div class="price">${product.price}</div>
+
+//                       <button>Add to Cart</button>`;
+
+
+
+//         gallery.appendChild(div);
+
+//     });
+
+// });
